307ca8a93822587d68adf2d3a56fd3460f69f5e5
aatauil <achrafatauil@hotmail.com>
2020-07-18 21:07:22 +0200

    Update config.js

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -3,10 +3,18 @@
 module.exports = {
     purge: [],
     theme: {
+          screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1440px',
+          },
+   
         container: {
             center: true,
             padding: '1rem'
           },
+
     },
     variants: {},
     plugins: [],
