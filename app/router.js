8628a5751b464a9c96a28580afd2b91b87313784
aatauil <achrafatauil@hotmail.com>
2020-07-18 21:06:02 +0200

    Update router.js

--- app/router.js
+++ app/router.js
@@ -8,5 +8,5 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('home', {path: '/'})
-  this.route('search', {path: '/Searchpage'})
+  this.route('search', {path: '/searchpage'})
 });
