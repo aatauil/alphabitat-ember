4c2b5919f0171a82dbfc45cfb04d805aaba82566
aatauil <achrafatauil@hotmail.com>
2020-07-27 14:22:38 +0200

    create compatibel object for query params

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -2,4 +2,26 @@ import Controller from '@ember/controller';
 
 export default class SearchController extends Controller {
     queryParams= ['buyRent', 'minPrice', "maxPrice", "regions"]
+
+    get myQuery(){
+
+        let obj = new Object();
+
+        if (this.buyRent == 1){
+            obj.sale = true
+            obj.rent = false
+        }
+
+        if (this.buyRent == 2){
+            obj.rent = true
+            obj.sale = false
+        }
+
+        
+        obj.minPrice = this.minPrice
+        obj.maxPrice = this.maxPrice
+        obj.regions = this.regions
+
+        return obj
+    }
 }
