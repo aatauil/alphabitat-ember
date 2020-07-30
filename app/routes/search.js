acd9f789a15e57492df77b1b09a6b4bb44ddf42d
aatauil <achrafatauil@hotmail.com>
2020-07-30 15:08:31 +0200

    api request now based on query param options + region logic

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,10 +1,42 @@
 import Route from '@ember/routing/route';
 import axios from 'axios';
 import ENV from 'alphabitat-ember/config/environment'
+import { typeOf } from '@ember/utils';
+
 
 export default class SearchRoute extends Route {
-    async model() {
-        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"RowsPerPage":20,"Language":"en-gb","displayStatusIdList":3}`)
+    async model(param) { 
+      console.log(param)
+
+        let regionParam = param.regions.split(",")
+
+        let regionList = []
+        
+        regionParam.forEach(element => {
+         switch (element) {
+            case '1': 
+              regionList.push(20341)
+              break;
+
+            case '2':
+              regionList.push(20705)
+              break;
+
+            case '3':
+              regionList.push(20706,20346,20347)
+              break;
+
+            case '4': 
+              regionList.push(51498)
+              break;
+            
+            case '5':
+              regionList.push(20347,20346,20345,20344,20343,20342)
+              break;
+         }
+        });
+        
+        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"RowsPerPage":20,"Language":"en-gb","PurposeIDList": [${param.buyRent}], "PriceRange":["${param.minPrice}", "${param.maxPrice}"}], "RegionIDList": [${regionList}] }`)
         const data = await response.data.d.EstateList
         await console.log(data)
         return data
