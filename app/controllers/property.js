9b3ec69e9b1272577cd31e2839698b998bf20ad6
aatauil <achrafatauil@hotmail.com>
2020-07-30 16:04:12 +0200

    added property query and linked it to API

---
+++ app/controllers/property.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+
+export default class PropertyController extends Controller {
+
+    queryParams = ["estateID"]
+
+}
--- app/routes/property.js
+++ app/routes/property.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import ENV from 'alphabitat-ember/config/environment' 
 
 export default class PropertyRoute extends Route {
-    async model() {
-        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"Language":"en-gb", "EstateID":4098403, "ShowDetails":true}`)
+    async model(param) {
+
+        let estateID = param.estateID
+        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"Language":"en-gb", "EstateID":${estateID}, "ShowDetails":true}`)
         const data = await response.data.d.EstateList
         const detailsArray = data[0].Details
 
