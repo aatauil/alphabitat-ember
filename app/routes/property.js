90701a8b9c2bf33cb26a5b5d03942ec13cdfec5a
aatauil <achrafatauil@hotmail.com>
2020-07-30 10:10:00 +0200

    import dummy property + seperate subDetails

--- app/routes/property.js
+++ app/routes/property.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
+import ENV from 'alphabitat-ember/config/environment' 
 
 export default class PropertyRoute extends Route {
+    async model() {
+        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"Language":"en-gb", "EstateID":4098403, "ShowDetails":true}`)
+        const data = await response.data.d.EstateList
+        const detailsArray = data[0].Details
+
+        let interior = detailsArray.filter((details) => details.Type == "interior")
+        let exterior = detailsArray.filter((details) => details.Type == "exterior")
+        let communication = detailsArray.filter((details) => details.Type == "communication")
+        return {data , interior, exterior, communication}
+      }
 }
