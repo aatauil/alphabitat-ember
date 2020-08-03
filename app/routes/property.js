6d09d86b23df45873c4fcfcf659c9c74212bb324
aatauil <achrafatauil@hotmail.com>
2020-08-03 14:31:35 +0200

    filter api data into categories

--- app/routes/property.js
+++ app/routes/property.js
@@ -10,9 +10,56 @@ export default class PropertyRoute extends Route {
         const data = await response.data.d.EstateList
         const detailsArray = data[0].Details
 
-        let interior = detailsArray.filter((details) => details.Type == "interior")
-        let exterior = detailsArray.filter((details) => details.Type == "exterior")
-        let communication = detailsArray.filter((details) => details.Type == "communication")
-        return {data , interior, exterior, communication}
+        console.log(data)
+
+        let general = detailsArray.filter(details => {
+          let generalIDList = [374, 31, 32, 573, 52, 465]
+
+            if (generalIDList.indexOf(details.DetailId) !== -1){
+              return true
+            }
+          }
+        )
+
+        let interior = detailsArray.filter(details => {
+          let interiorIDList = [94, 88, 96]
+
+            if (interiorIDList.indexOf(details.DetailId) !== -1){
+              return true
+            }
+          }
+        )
+
+
+        let surface = detailsArray.filter(details => {
+          let surfaceIDList = [1028, 73, 117, 118, 119]
+
+          if (surfaceIDList.indexOf(details.DetailId) !== -1){
+            console.log(details.DetailId)
+            return true
+          }
+        })
+
+        let energy = detailsArray.filter(details => {
+          let energyIDList = [1307]
+
+          if (energyIDList.indexOf(details.DetailId) !== -1){
+            return true
+          }
+        }
+        )
+
+        let environment= detailsArray.filter(details => {
+          let envIDList = [925, 1180, 150, 151, 152, 158]
+
+          if (envIDList.indexOf(details.DetailId) !== -1){
+            return true
+          }
+        }
+        )
+
+
+
+        return {data , general, interior, energy, surface, environment}
       }
 }
