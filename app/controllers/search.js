1343cbbdfce181d43cdd6babf63e912c04502ac9
aatauil <achrafatauil@hotmail.com>
2020-08-04 13:03:57 +0200

    add all params for filter inside controller

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,12 +1,23 @@
 import Controller from '@ember/controller';
 
 export default class SearchController extends Controller {
-    queryParams= ['buyRent', 'minPrice', "maxPrice", "regions", "minBed", "maxBed"]
+    queryParams= ['buyRent', 'minPrice', "maxPrice", "regions", "minBed", "maxBed", "property", "minBath", "maxBath", "area"]
 
     buyRent = 1;
+
     minPrice = "0";
     maxPrice = "9999999";
+
     regions = "1,2,3,4,5"
+
+    minBed =  "0";
+    maxBed = "20"
+
+    property = '1,2,3,4,5,6,7'
+
+    area = '0'
+
+
 
     get myQuery(){
 
@@ -26,6 +37,14 @@ export default class SearchController extends Controller {
         obj.minPrice = this.minPrice
         obj.maxPrice = this.maxPrice
         obj.regions = this.regions
+
+        obj.minBed = this.minBed
+        obj.maxBed = this.maxBed
+
+        obj.property = this.property
+
+        obj.area = this.area
+
 
         return obj
     }
