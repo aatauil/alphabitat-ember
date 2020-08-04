4c7b5ac8df0bfcb4617841f1c4754eddfa72dad3
aatauil <achrafatauil@hotmail.com>
2020-08-04 13:03:29 +0200

    finished favorites page

--- app/routes/favorites.js
+++ app/routes/favorites.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
+import ENV from 'alphabitat-ember/config/environment'
 
 export default class FavoritesRoute extends Route {
+
+    async model() {
+        let favoritesList = window.localStorage.getItem("favorites")
+        console.log(window.localStorage.getItem('favorites') != "[]")
+
+        if(window.localStorage.getItem('favorites') && window.localStorage.getItem('favorites') != "[]"){
+            const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Language":"en-gb","Page":0,"RemoveRowLimit":true, "EstateIDList": ${favoritesList}}`)
+            const data = await response.data.d.EstateList
+            console.log(data)
+            return data
+        } else {
+            return false
+        }
+
+      
+        
+      }
+
 }
