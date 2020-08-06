2817c6c87f1af1dd54901599c27fe6add94512a9
aatauil <achrafatauil@hotmail.com>
2020-08-06 12:35:49 +0200

    clean up code (spaces)

--- app/components/property/main.js
+++ app/components/property/main.js
@@ -4,8 +4,13 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class PropertyMainComponent extends Component {
+
     @service('favorites') favorites;
 
+    favoritesList = JSON.parse(window.localStorage.getItem('favorites')) || []
+
+
+    // ACTIONS 
     @action
     addRemove(id){
         this.favorites.addRemove(id)
@@ -13,24 +18,35 @@ export default class PropertyMainComponent extends Component {
 
     @action
     showList(){
-        this.favorites.showList()
+        console.log(this.args.estate.EstateID)
     }
 
-    favoritesList = JSON.parse(window.localStorage.getItem('favorites')) || []
-    
+
+    // GETTERS
+    get favoriteActive(){
+        let itemIndex = this.favoritesList.indexOf(this.args.estate.EstateID)
+
+        if(itemIndex == -1){
+            return false
+        } else {
+            return true
+        }
+    }
+
+    // FUNCTIONS
     addRemove(id){
 
         let itemIndex = this.favoritesList.indexOf(id)
-
         if(itemIndex == -1){
 
+            this.favoriteActive = true
             // Item does not exist in array, item will be added
             this.favoritesList.push(id)
             console.log("item has been added")
 
         } else {
             // Item does exist in array, item will be removed
-
+            this.favoriteActive = false
             this.favoritesList.splice(itemIndex, 1)
             console.log("item has been removed")
         }
