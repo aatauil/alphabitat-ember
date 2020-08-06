d38b87784353c2a41f935770c8490fe3fc632664
aatauil <achrafatauil@hotmail.com>
2020-08-06 12:32:37 +0200

    remove mobile nav

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -3,8 +3,11 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class NavBarComponent extends Component {
+
+  // TRACKED
   @tracked menuOpenState = false;
-  
+
+  //ACTIONS
   @action toggleMenu(){
       this.menuOpenState = !this.menuOpenState
   }
