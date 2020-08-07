f73719d5b83ab1a7312febbaab749ff1a012acb3
aatauil <achrafatauil@hotmail.com>
2020-08-07 11:49:27 +0200

    refactored Hero setion - OOP

--- app/components/home/quick-search.js
+++ app/components/home/quick-search.js
@@ -2,39 +2,19 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class HomeQuickSearchComponent extends Component {
 
+// MANAGES THE OPEN STATE OF EACH DROPDOWN ELEMENT=========
+class DropDownClass {
+    @tracked purpose;
+    @tracked price;
+    @tracked region;
 
-    // Open state of dropdowns
-    @tracked purpose = false;
-    @tracked price = false;
-    @tracked region = false;
-
-    // Button content Purpose
-    @tracked purposeState = "For sale"
-    @tracked radioForSale = true;
-    @tracked radioForRent = false;
-
-    @tracked buyRent = 1;
-
-     // Button content Region
-    @tracked cbRegionAll = false
-    @tracked cbBrux = false
-    @tracked cbFlem = false
-    @tracked cbWall = false
-    @tracked cbFlan = false
-    @tracked cbOther = false
-
-    @tracked regionList = [];
-
-    // Button select price
-
-    @tracked minBudget = 0 
-    @tracked maxBudget = 9999999
-
-
-    // Dropdown open state logic
-
+    constructor(){
+        this.purpose = false;
+        this.price = false;
+        this.region = false;
+    }
+    
     @action togglePurpose(){
         this.purpose = !this.purpose;
         this.price = false;
@@ -46,53 +26,112 @@ export default class HomeQuickSearchComponent extends Component {
         this.purpose = false;
         this.region = false ;
     }
-
+    
     @action toggleRegion(){
         this.region = !this.region;
         this.price = false;
         this.purpose = false ;
     }
 
-    // Closes all dropdowns
-
     @action toggleAll(){
         this.price = false;
-        this.purpose = false ;
+        this.purpose = false;
         this.region = false ;
     }
+}
 
-    // PURPOSE LOGIC
+// MANAGES PURPOSE DROPDOWN STATE===========================
+class PurposeClass {
+    @tracked purposeState;
+    @tracked radioForSale;
+    @tracked radioForRent;
+    @tracked buyRent;
+
+    constructor(){
+        this.purposeState = "For sale";
+        this.radioForSale = true;
+        this.radioForRent = false;
+        this.buyRent = 1;
+    }
 
     @action setPurpose(element){
-        this.updateState(element)
-        this.purposeState = element
+        this.updateState(element);
+        this.purposeState = element;
     }
 
-    regionAllLogic(){
+    
+    updateState(element){
+        if(element == "For sale"){
+            this.radioForSale = true;
+            this.radioForRent = false;
+            this.buyRent = 1;
+        }
 
-        if (!this.cbBrux || !this.cbFlem || !this.cbWall || !this.cbFlan || !this.cbOther){
-        this.cbRegionAll = false
-        
+        if(element == "For rent"){
+            this.radioForSale = false;
+            this.radioForRent = true;
+            this.buyRent = 2;
+        }
+    }
+}
+
+// MANAGES PRICE DROPDOWN STATE============================
+class PriceClass {
+
+    @tracked minBudget;
+    @tracked maxBudget;
+
+    constructor(){
+        this.minBudget = 0;
+        this.maxBudget = 999999;
     }
 
-    if (this.cbBrux && this.cbFlem && this.cbWall && this.cbFlan && this.cbOther){
-        this.cbRegionAll = true
-        this.checkAllRegion()
-        
-        }   
+    // PRICE LOGIC
+    @action updateMinBudget(event){
+        this.minBudget = event.target.value;
+    }
+
+    @action updateMaxBudget(event){
+        this.maxBudget = event.target.value;
+    }
+}
+
+// MANAGES REGION DROPDOWN STATE===========================
+class RegionClass {
+
+    @tracked cbRegionAll;
+    @tracked cbBrux;
+    @tracked cbFlem;
+    @tracked cbWall;
+    @tracked cbFlan;
+    @tracked cbOther;
+    @tracked regionList;
+    
+    constructor(){
+        this.cbRegionAll = false;
+        this.cbBrux = false;
+        this.cbFlem = false;
+        this.cbWall = false;
+        this.cbFlan = false;
+        this.cbOther = false;
+        this.regionList = [];
     }
     
+    @action regionAllLogic(){
 
-    // ---------------------------------------------------------------------------------------------------
+        if (!this.cbBrux || !this.cbFlem || !this.cbWall || !this.cbFlan || !this.cbOther){
+        this.cbRegionAll = false;
+        }
 
-    // REGION LOGIC
-
-    @action verifyRegion(element, event){
- 
-        this.updateState(element, event.target.checked, event.target.value)
-
+        if (this.cbBrux && this.cbFlem && this.cbWall && this.cbFlan && this.cbOther){
+            this.cbRegionAll = true;
+            this.checkAllRegion();
+        }   
     }
 
+    @action verifyRegion(element, event){
+        this.updateState(element, event.target.checked, event.target.value);
+    }
 
     checkAllRegion(){
         this.cbBrux = true;
@@ -112,112 +151,89 @@ export default class HomeQuickSearchComponent extends Component {
 
     regionArray(state, value){
         if(state == true ){
-            this.addRegion(value)
+            this.addRegion(value);
         } else if (state == false){
-            this.removeRegion(value)
+            this.removeRegion(value);
         }
-        
     }
 
     addRegion(value){
-        let split = value.split(",")
-        this.regionList.push(split)
-        console.log("regionlist added =" + this.regionList)
+        let split = value.split(",");
+        this.regionList.push(split);
+        console.log("regionlist added =" + this.regionList);
     }
 
     removeRegion(value){
         const index = this.regionList.indexOf(value);
         if(index != -1){
-            this.regionList.splice(index, 1)
-         
+            this.regionList.splice(index, 1);
         }
         console.log("regionlist spliced =" + this.regionList)
     }
 
     addAllRegion(){
-        this.regionList = ["1", "2", "3", "4", "5"]
+        this.regionList = ["1", "2", "3", "4", "5"];
     }
 
     removeAllRegion(){
-        this.regionList = []
+        this.regionList = [];
     }
-
-
-
-    // PRICE LOGIC
-
-    @action updateMinBudget(event){
-      
-        this.minBudget = event.target.value
-    }
-
-    @action updateMaxBudget(event){
-    
-        this.maxBudget = event.target.value
-    }
-
-
-    // Shared Logic
 
     updateState(element, state, value){
-
-        // Radio
-
-        if(element == "For sale"){
-            this.radioForSale = true
-            this.radioForRent = false
-            this.buyRent = 1
-        }
-
-        if(element == "For rent"){
-            this.radioForRent = true
-            this.radioForSale = false
-            this.buyRent = 2
-        }
-
-        // Dropdown
-
         if (element == "cbRegionAll"){
-            this.cbRegionAll = state
+            this.cbRegionAll = state;
             if (this.cbRegionAll == true) {
-                this.addAllRegion()
-                this.checkAllRegion()
+                this.addAllRegion();
+                this.checkAllRegion();
             } else {
-                this.removeAllRegion()
-                this.unCheckAllRegion()
+                this.removeAllRegion();
+                this.unCheckAllRegion();
             }
         }
 
         if (element == "cbBrux"){
-            this.cbBrux = state
-            this.regionArray(state, value)
-            this.regionAllLogic()
+            this.cbBrux = state;
+            this.regionArray(state, value);
+            this.regionAllLogic();
         }
 
         if (element == "cbFlem"){
-            this.cbFlem = state
-            this.regionArray(state, value)
-            this.regionAllLogic()
+            this.cbFlem = state;
+            this.regionArray(state, value);
+            this.regionAllLogic();
         }
 
         if (element == "cbWall"){
-            this.cbWall = state
-            this.regionArray(state, value)
-            this.regionAllLogic()
+            this.cbWall = state;;
+            this.regionArray(state, value);;
+            this.regionAllLogic();;
         }
 
         if (element == "cbFlan"){
-            this.cbFlan = state
-            this.regionArray(state, value)
-            this.regionAllLogic()
+            this.cbFlan = state;
+            this.regionArray(state, value);
+            this.regionAllLogic();
         }
 
         if (element == "cbOther"){
-            this.cbOther = state
-            this.regionArray(state, value)
-            this.regionAllLogic()
+            this.cbOther = state;
+            this.regionArray(state, value);
+            this.regionAllLogic();
         }
-        
     }
+}
+
+// =====================================================================
+
+
+export default class HomeQuickSearchComponent extends Component {
+
+    // Dropdown Open/Closed State Handler
+    @tracked Dropdown = new DropDownClass()
+
+    // Individual dropdown element handlers
+    @tracked Purpose = new PurposeClass()
+    @tracked Price = new PriceClass()
+    @tracked Region = new RegionClass()
 
 }
