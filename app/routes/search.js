83a580653ce9c7788c10e76265e00f3dc00c8048
aatauil <achrafatauil@hotmail.com>
2020-08-09 16:31:13 +0200

    add refetch models  & update url

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,56 +1,53 @@
 import Route from '@ember/routing/route';
 import axios from 'axios';
 import ENV from 'alphabitat-ember/config/environment'
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 
 export default class SearchRoute extends Route {
 
 
   // SEARCH QUERY PARAMS
-  queryParams = {
-    buyRent: {
-      refreshModel: true
-    }
-  };
+    queryParams = {
+      buyRent: {
+        refreshModel: true
+      },
+      category: {
+        refreshModel: true
+      },
+      region: {
+        refreshModel: true
+      },
+      minBed: {
+        refreshModel: true
+      },
+      minBath: {
+        refreshModel: true
+      },
+      minArea: {
+        refreshModel: true
+      },
+      order: {
+        refreshModel: true
+      }
+    };
   
     async model(param) { 
-      console.log(param)
+        let purposeID = param.buyRent
+        let categoryList = param.category 
+        let regionList = param.regions
+        let minBedrooms = param.minBed
+        let minBathrooms = param.minBath
+        let minArea = param.minArea
+        let order = param.order
 
-        let regionParam = param.regions.split(",") 
 
-        let regionList = new Array
         
-        regionParam.forEach(element => {
-         switch (element) {
-            case '1': 
-              regionList.push(20341)
-              break;
-
-            case '2':
-              regionList.push(20705)
-              break;
-
-            case '3':
-              regionList.push(20706,20346,20347)
-              break;
-
-            case '4': 
-              regionList.push(51498)
-              break;
-            
-            case '5':
-              regionList.push(20347,20346,20345,20344,20343,20342)
-              break;
-              
-         }
-        });
-        console.log(regionList)
-        
-        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"RowsPerPage":20,"Language":"en-gb","PurposeIDList": [${param.buyRent}], "PriceRange":["${param.minPrice}", "${param.maxPrice}"}], "RegionIDList": [${regionList}] }`)
+        const response = await axios.get(`${ENV.APP.API_URL}{"ClientId":"${ENV.APP.API_TOKEN}","Page":0,"Language":"en-gb","PurposeIDList": [${ purposeID || 1 }],"CategoryIDList":[${ categoryList || "" }], "RegionIDList": [${ regionList || "" }], "MinRooms": ${minBedrooms || null}, "MinBathRooms":${minBathrooms || null}, "AreaRange": [${minArea || 0}, 1000], "OrderByFields":["${order || ""}"] }`)
         const data = await response.data.d.EstateList
         await console.log(data)
         return data
       }
 }
+
+
+// "PriceRange":["${param.minPrice}", "${param.maxPrice}"}],
