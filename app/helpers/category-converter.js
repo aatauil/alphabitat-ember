23c515a87266c7826b8dff0a02516e9d638bd08c
aatauil <achrafatauil@hotmail.com>
2020-08-09 16:31:24 +0200

    add remaining categories

--- app/helpers/category-converter.js
+++ app/helpers/category-converter.js
@@ -12,6 +12,18 @@ export default helper(function categoryConverter([id]) {
                 case 3:
                     transformed = 'Villa'
                     break;
+                case 4:
+                transformed = 'Office'
+                break;
+                case 5:
+                    transformed = 'Store'
+                    break;
+                case 6:
+                    transformed = 'Industrial'
+                    break;
+                case 7:
+                    transformed = 'Garage/parking'
+                    break;
                 default:
                     break;
             }
