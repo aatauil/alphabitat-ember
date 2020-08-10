6aae464b0d5e678dcdff834af91c3a8f0779ab78
aatauil <achrafatauil@hotmail.com>
2020-08-10 10:16:11 +0200

    color favorite based on active state

--- app/components/property/main.js
+++ app/components/property/main.js
@@ -3,14 +3,60 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class PropertyMainComponent extends Component {
+
+class FavoritesClass{
 
     @service('favorites') favorites;
+    @tracked propertyID
+    @tracked favoritesList
+    @tracked active
 
-    favoritesList = JSON.parse(window.localStorage.getItem('favorites')) || []
+    constructor(id, list){
+        this.propertyID = id
+        this.favoritesList = list || []
+        this.active = this.checkActive()
+    }
+
+    @action addRemove(id){
+
+        let itemIndex = this.favoritesList.indexOf(this.propertyID)
+        if(itemIndex == -1){
+            // Item does not exist in array, item will be added
+            this.active = true
+            this.favoritesList.push(id)
+            console.log("item has been added")
+
+        } else {
+            // Item does exist in array, item will be removed
+            this.active = false
+            this.favoritesList.splice(itemIndex, 1)
+            console.log("item has been removed")
+        }
+
+        window.localStorage.setItem("favorites" , JSON.stringify(this.favoritesList))
+
+    }
+
+    checkActive(){
+        let itemIndex = this.favoritesList.indexOf(this.propertyID)
+
+        if(itemIndex == -1){
+            return false
+        } else {
+            return true
+        }
+    }
 
 
-    // ACTIONS 
+    
+}
+
+export default class PropertyMainComponent extends Component {
+
+
+    @tracked Favorite = new FavoritesClass(this.args.estate.EstateID, JSON.parse(window.localStorage.getItem('favorites')))
+
+
     @action
     addRemove(id){
         this.favorites.addRemove(id)
@@ -22,37 +68,11 @@ export default class PropertyMainComponent extends Component {
     }
 
 
-    // GETTERS
-    get favoriteActive(){
-        let itemIndex = this.favoritesList.indexOf(this.args.estate.EstateID)
+    // ACTIONS 
+  
 
-        if(itemIndex == -1){
-            return false
-        } else {
-            return true
-        }
-    }
 
     // FUNCTIONS
-    addRemove(id){
 
-        let itemIndex = this.favoritesList.indexOf(id)
-        if(itemIndex == -1){
-
-            this.favoriteActive = true
-            // Item does not exist in array, item will be added
-            this.favoritesList.push(id)
-            console.log("item has been added")
-
-        } else {
-            // Item does exist in array, item will be removed
-            this.favoriteActive = false
-            this.favoritesList.splice(itemIndex, 1)
-            console.log("item has been removed")
-        }
-
-        window.localStorage.setItem("favorites" , JSON.stringify(this.favoritesList))
-
-    }
     
 }
