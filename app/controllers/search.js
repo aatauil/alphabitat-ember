785841a18de600bd6f0a569de47631d262dda040
achraf <achrafatauil@hotmail.com>
2020-09-05 23:49:24 +0200

    Change default query param values

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -44,12 +44,13 @@ import { action } from '@ember/object';
 
 export default class SearchController extends Controller {
     
-    queryParams= ['buyRent', 'minPrice', "maxPrice"];
+    queryParams= ['buyRent', 'minPrice', "maxPrice", "regions"];
 
     //TRACKED
     @tracked buyRent = 1
-    @tracked minPrice = 0
-    @tracked maxPrice = 9999999
+    @tracked minPrice = ""
+    @tracked maxPrice = ""
+    @tracked regions = ""
 
 
     // ACTIONS
@@ -66,6 +67,7 @@ export default class SearchController extends Controller {
         obj.buyRent = this.buyRent;
         obj.minPrice = this.minPrice;
         obj.maxPrice = this.maxPrice;
+        obj.regions = this.regions
 
         return obj
     }
