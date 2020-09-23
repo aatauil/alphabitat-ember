5eecbde0819dfbcbfef66fbb2e581f8e37ee9c0a
achraf <achrafatauil@hotmail.com>
2020-09-23 19:59:56 +0200

    pagination logic added (no arrows yet)

--- app/components/search/results.js
+++ app/components/search/results.js
@@ -2,7 +2,39 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking'
 
+class Navigation {
+
+  @tracked resultCount
+  @tracked currentPage;
+  @tracked maxPage;
+  @tracked pages;
+  @tracked list = []
+
+  constructor(context){
+    this.context = context
+    this.resultCount = this.context.meta.RowCount
+    this.currentPage = this.context.query.page || 0
+    this.maxPage = Math.ceil(this.context.meta.RowCount / 10)
+    this.calculateList()
+  }
+
+  @action refetch(val){
+    if(val != (this.maxPage + 1) || val != 0){
+      this.context.refetch("page", val)
+    }
+  }
+
+  calculateList(){
+    for(let i = 0; i < this.maxPage; i++){
+      this.list.push(i + 1)
+
+    }
+    console.log(this.list)
+  }
+}
+
 export default class SearchResultsComponent extends Component {
+  
 
     @tracked order = decodeURIComponent(this.args.query.order) || "";
     @tracked selectedOrder = this.order
@@ -15,6 +47,8 @@ export default class SearchResultsComponent extends Component {
       { name: "Largest Area", value: "Area DESC" },
     ]
 
+
+    @tracked Navigation = new Navigation(this.args)
     // Gets Value from event target & sends refetch request to controller
     @action invokeRefetch(elem, e){
 
