68a79bc936e0c6a322937ad1f18d6b69318d6b23
achraf <achrafatauil@hotmail.com>
2020-11-05 17:45:44 +0100

    add setLanguage function

--- app/components/header.js
+++ app/components/header.js
@@ -1,5 +1,13 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class HeaderComponent extends Component {
+  @service intl;
 
+  @action setLang(lang){
+
+    this.intl.set('locale', lang)
+
+  }
 }
