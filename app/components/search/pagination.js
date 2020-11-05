f90b6f1f85e078e742611ccd6c50d9141eccda0d
achraf <achrafatauil@hotmail.com>
2020-11-05 15:14:04 +0100

    Add better pagination logic

--- app/components/search/pagination.js
+++ app/components/search/pagination.js
@@ -1,4 +1,16 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class SearchPaginationComponent extends Component {
+
+  @action previousPage(page){
+    let previous = page -1
+    this.args.nav.context.refetch("page", previous)
+  }
+
+  @action nextPage(page){
+    let next = page + 1
+    console.log(page)
+    this.args.nav.context.refetch("page", next)
+  }
 }
