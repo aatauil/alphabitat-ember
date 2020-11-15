8e7973fb6c47d7172fada23893db965abe75352f
achraf <achrafatauil@hotmail.com>
2020-11-15 12:25:19 +0100

    Refresh model when language changed

--- app/components/header.js
+++ app/components/header.js
@@ -1,14 +1,18 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import getOwner from "ember-owner/get";
 
 export default class HeaderComponent extends Component {
   @service intl;
+  @service router;
 
   @action setLang(lang){
 
     this.intl.set('locale', lang)
     localStorage.setItem("userLanguage", lang);
+    const currentRouteInstance = getOwner(this).lookup(`route:${this.router.currentRouteName}`);
+    currentRouteInstance.refresh();
 
   }
   
