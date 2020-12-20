5bf238382685a2a86dae066514cefb11c5cab0e5
achraf <achrafatauil@hotmail.com>
2020-12-20 11:23:41 +0100

    add error handler to home + axios instead of fetch

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,17 +1,25 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import axios from 'axios';
 
 export default class HomeController extends Controller {
   @service intl;
+  @tracked isError = false;
 
   get currentLang() {
     return this.intl.get('primaryLocale')
   }
 
   get estates(){
-    return fetch(`/.netlify/functions/estate-request?params={"ClientId":"API_TOKEN","Page":0,"RowsPerPage":6,"Language":"${this.currentLang}","displayStatusIdList":3}`)
-      .then(responds => responds.json())
-      .then(data => data.d.EstateList)
+    return axios.get(`/.netlify/functions/estate-request?params={"ClientId":"API_TOKEN","Page":0,"RowsPerPage":6,"Language":"${this.currentLang}","displayStatusIdList":3}`)
+      .then(responds => responds.data.d.EstateList)
+  }
+
+  @action
+    handleError(){
+      this.isError = true;
     }
 }
 
