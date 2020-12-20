a92302191f82f147e557e7cefe6c4b4aaa176b55
achraf <achrafatauil@hotmail.com>
2020-12-20 11:23:55 +0100

    remove env and console.logs

--- app/routes/favorites.js
+++ app/routes/favorites.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import axios from 'axios';
-import ENV from 'alphabitat-ember/config/environment'
 import { inject as service } from '@ember/service';
 
 export default class FavoritesRoute extends Route {
@@ -12,12 +11,10 @@ export default class FavoritesRoute extends Route {
 
   async model() {
       let favoritesList = window.localStorage.getItem("favorites")
-      console.log(window.localStorage.getItem('favorites') != "[]")
 
       if(window.localStorage.getItem('favorites') && window.localStorage.getItem('favorites') != "[]"){
           const response = await axios.get(`/.netlify/functions/estate-request?params={"ClientId":"API_TOKEN","Language":"${this.currentLang}","Page":0,"RemoveRowLimit":true, "EstateIDList": ${favoritesList}}`)
           const data = await response.data.d.EstateList
-          // console.log(data)
           return data
       } else {
           return false
