6a764a13f78b23a521591dc35af925fc551ff331
Unknown <achrafatauil@hotmail.com>
2021-04-03 14:46:29 +0200

    fix lang refresh when changing for mobile lang btns

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,13 +2,15 @@ import Component from '@glimmer/component';
 import {tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import getOwner from "ember-owner/get";
 
 export default class NavBarComponent extends Component {
   @service intl;
   @tracked openState = false;
+  @service router;
 
   @action toggleMenu(){
-    this.openState = !this.openState 
+    this.openState = !this.openState
     let body = document.querySelector('body');
         if(this.openState){
             body.classList.add("overflow-hidden");
@@ -16,11 +18,15 @@ export default class NavBarComponent extends Component {
             body.classList.remove("overflow-hidden");
         }
   }
-  
+
   @action setLang(lang){
     this.intl.set('locale', lang)
+    localStorage.setItem("userLanguage", lang);
+    const currentRouteInstance = getOwner(this).lookup(`route:${this.router.currentRouteName}`);
+    currentRouteInstance.refresh();
+    this.toggleMenu()
   }
-  
+
   get currentLang() {
     return this.intl.get('primaryLocale')
   }
