f7bb131a37e11d421c5c9485957bd594d381ce0a
Achraf <achrafatauil@hotmail.com>
2025-09-20 13:01:28 +0200

    fix parseprice (type changed)

--- app/helpers/parse-price.js
+++ app/helpers/parse-price.js
@@ -1,10 +1,9 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function parsePrice([price]) {
-    return (
-        price.replace(
-        /(\d{1,3})?(\d{3})(\.\d{2})/,
-        " $1 $2"
-        )
-    )
+  if (typeof price !== 'number') {
+    return '';
+  }
+  // Format without decimals and use spaces as thousand separator
+  return price.toLocaleString('fr-FR', { minimumFractionDigits: 0, maximumFractionDigits: 0 });
 });
