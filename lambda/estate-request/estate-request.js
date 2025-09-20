032ec1b6e669e0430a0c084b1a609f98a5925578
Achraf <achrafatauil@hotmail.com>
2025-09-20 10:50:53 +0200

    add better logging and remove params from sever frunction

--- lambda/estate-request/estate-request.js
+++ lambda/estate-request/estate-request.js
@@ -3,18 +3,23 @@ const axios = require('axios')
 const handler = async function (event) {
   const { API_URL, API_TOKEN } = process.env
 
-  const URL = `${API_URL}=${params}`
+  const axiosConfig = {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${API_TOKEN}`
+    }
+  }
 
-  console.log('Constructed URL is ...', URL)
+  // Log constructed URL after all headers & body have been added
+  console.log('Constructed URL is ...', API_URL)
+  console.log('Request headers:', axiosConfig.headers)
+  if (event.body) {
+    console.log('Request body:', event.body)
+  }
 
   try {
-    const { data } = await axios.get(URL, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${API_TOKEN}`
-      }
-    })
+    const { data } = await axios.get(API_URL, axiosConfig)
 
     return {
       statusCode: 200,
