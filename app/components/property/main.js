731d2f417d735e2e843de73f849c1be241542a29
Achraf <achrafatauil@hotmail.com>
2025-09-20 14:59:55 +0200

    update main details

--- app/components/property/main.js
+++ app/components/property/main.js
@@ -53,35 +53,48 @@ export default class PropertyMainComponent extends Component {
     )
 
 
-  get propertyType(){
-    let transformed;
-    switch (this.args.estate.CategoryId) {
-      case 1:
-          transformed = this.intl.t('type.house');
-          break;
-      case 2:
-          transformed = this.intl.t('type.flat')
-          break;
-      case 3:
-          transformed = "Villa"
-          break;
-      case 4:
-          transformed = this.intl.t('type.office')
-          break;
-      case 5:
-          transformed = this.intl.t('type.store')
-          break;
-      case 6:
-          transformed = this.intl.t('type.industrial')
-          break;
-      case 7:
-          transformed = this.intl.t('type.garage')
-          break;
-      default:
-          break;
-  }
+    get propertyType(){
+        let transformed;
+        switch (this.args.estate.category.id) {
+            case 1:
+                transformed = this.intl.t('type.house');
+                break;
+            case 2:
+                transformed = this.intl.t('type.flat')
+                break;
+            case 3:
+                transformed = "Villa"
+                break;
+            case 4:
+                transformed = this.intl.t('type.office')
+                break;
+            case 5:
+                transformed = this.intl.t('type.store')
+                break;
+            case 6:
+                transformed = this.intl.t('type.industrial')
+                break;
+            case 7:
+                transformed = this.intl.t('type.garage')
+                break;
+            default:
+                break;
+            }
+            return transformed
+        }
 
-  return transformed
-  }
-
+        get purposeType(){
+        let transformed;
+        switch (this.args.estate.purpose.id) {
+            case 1:
+                transformed = this.intl.t('nav.sale');
+                break;
+            case 2:
+                transformed = this.intl.t('nav.rent')
+                break;
+            default:
+                break;
+            }
+            return transformed ? transformed.toLowerCase() : transformed
+        }
 }
